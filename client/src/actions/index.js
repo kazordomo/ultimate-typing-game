6026efd2b1baf5873aee54b12ee3b43a69de015a
kazordomo <zak.eklund@live.se>
2018-02-13 23:10:17 +0100

    refactored and fixed fetchUser action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,9 @@
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
+export const fetchUser = () => async dispatch => {
     //redux-thunks - dispatch function when request is done.
-    return function(dispatch) {
-        //refactor to async/await
-        fetch('/api/current_user').then(response => {
-            return response.json();
-        }).then(data => {
-            dispatch({ type: FETCH_USER, payload: data });
-        });
-    }
+    //"credentials include" is need to get the user out of the passport session. 1h debugging. yay.
+    const response = await fetch('/api/current_user', {credentials: 'include'});
+    const json = await response.json();
+    dispatch({ type: FETCH_USER, payload: json });
 };
