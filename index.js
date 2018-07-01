d930795acab31c0e21c4e8ef58cad498e453e3f6
kazordomo <zak.eklund@live.se>
2018-07-01 12:28:01 +0200

    check if NODE_ENV === production

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.use(passport.session()); //tell passport to handle our session.
 require('./routes/authRoutes')(app);
 require('./routes/genericRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 app.set('port', port);
 server.listen(port);
